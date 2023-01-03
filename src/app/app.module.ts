f1ecd9ea22cff41d76c0761a8b4ade819d692dbf
marmar <marianamartyniuk2001@gmail.com>
2023-01-03 11:01:49 +0100

    add spinner/interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { MainPageModule } from './features/main-page/main-page.module';
 import {CommonModule} from "@angular/common";
 import {CustomPipe} from "./core/pipe/custom-pipe.pipe";
+import { SpinnerComponent } from './core/spinner/spinner.component';
+import {LoadingInterceptor} from "./core/loading.interceptor";
+
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule, MainPageModule, CommonModule],
-  declarations: [AppComponent, CustomPipe],
+  declarations: [AppComponent, CustomPipe, SpinnerComponent],
   bootstrap: [AppComponent],
+  //https://danielk.tech/home/angular-how-to-add-a-loading-spinner
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true
+    }
+  ],
 })
 export class AppModule {}
